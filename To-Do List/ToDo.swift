5aae66f6cb77dbdd19b4a863c70b69b5ef438d25
Mahee <maheepatel2026@gmail.com>
2022-08-16 10:33:55 -0700

    Initial Commit

---
+++ To-Do List/ToDo.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo.swift
+//  To-Do List
+//
+//  Created by Mahee Patel on 2022-08-15.
+//
+
+import UIKit
+class ToDo {
+    var name = ""
+    var important = false
+    
+    init(ToDoName:String, ToDoImportant:Bool) {
+    
+    }
+    
+}
---
+++ To-Do List/ToDoTableViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ToDoTableViewController.swift
+//  To-Do List
+//
+//  Created by Mahee Patel on 2022-08-15.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    var toDos : [ToDo] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        toDos = createToDos()
+    }
+    
+    func createToDos() -> [ToDo] {
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the dog"
+        
+        return [swift,dog]
+    }
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important {
+            cell.textLabel?.text = "!" + toDo.name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
